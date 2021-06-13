ceb4449475e03601076544bb55198515532f39db
Jefferson R. Melo <82545165+rmelojefferson@users.noreply.github.com>
2021-06-13 19:29:30 -0300

    Interação da página com o script
    - Integração das funcionalidades de gerar senhas de 8 ou 16 caracteres com o documento HTML.
    
    - Falta ajuste de quebras de linhas para senhas geradas e tirar senhas geradas do campo do id HTML "newPassword".

--- passwordGenerator.js
+++ passwordGenerator.js
@@ -1,33 +1,39 @@
 let caracteres = [0,1,2,3,4,5,6,7,8,9,'!','@','#','$','%','¨','&','*','(',')','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',"'",'"','(',')','´','`','{','}','[',']','~','^','?',',','.','<','>',';','/','|','+','-']
 
-let passLength = false;
-let recebeSenha = ''
+let recebeSenha = '';
+let senha = '';
+let senhaGerada = document.getElementById("generatedPass");
+let novaSenha = document.getElementById("newPassword");
 
+function newPass(){
+    let check = document.getElementById("passwordLength").checked;
+    if(check==true){
+        pass8();
+    }else{
+        pass16();
+    }
+}
 function pass8(){
     for(let c=0;c<8;c++){
-        let senha = ''
         let randomCaracter = Math.floor(Math.random()*caracteres.length);
         //variável que armazena um número que representará um caractere do array caracteres.
         senha = caracteres[randomCaracter];    
         recebeSenha = senha+recebeSenha;
     }
-    return recebeSenha
-}
+    novaSenha.innerHTML = recebeSenha;    
+    senhaGerada.innerHTML = recebeSenha;
 
-function pass16(){
+}
+function pass16(){    
     for(let c=0;c<16;c++){
-        let senha = ''
         let randomCaracter = Math.floor(Math.random()*caracteres.length);
         senha = caracteres[randomCaracter];    
         recebeSenha = senha+recebeSenha;    
     }
-    return recebeSenha
+    novaSenha.innerHTML = recebeSenha;
+    senhaGerada.innerHTML = recebeSenha;
 }
 
-if(passLength==true){
-    pass8()
-    console.log(recebeSenha)
-}else{
-    pass16()
-    console.log(recebeSenha)
-}
+/*
+- Precisa inserir quebra de linha a cada senha gerada no campo "Suas senhas"
+- Inserir a nova senha no lugar de "Clique no botão para gerar uma nova senha:" e fazer ele limpar cada vez que o botão for clicado novamente.*/

7eae248b6cca919957a77c13fe3723b74c1bdd32
Jefferson R. Melo <82545165+rmelojefferson@users.noreply.github.com>
2021-06-14 16:03:23 -0300

    Redução do código
    Transformação do array em um string, substituindo os indíces pelo tamanho(length) da string e assim, encurtar o código.

--- passwordGenerator.js
+++ passwordGenerator.js
@@ -1,7 +1,6 @@
-let caracteres = [0,1,2,3,4,5,6,7,8,9,'!','@','#','$','%','¨','&','*','(',')','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',"'",'"','(',')','´','`','{','}','[',']','~','^','?',',','.','<','>',';','/','|','+','-']
+let caracteres = `0123456789!@#$%¨&*abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'"()´{}[]~^?,.<>;/|+-`;
 
 let recebeSenha = '';
-let senha = '';
 let novaSenha = document.getElementById("newPassword");
 let senhasGeradas = document.getElementById("generatedPass");
 let contador = '';
@@ -19,9 +18,7 @@ function newPassButton(){
     recebeSenha= '';
     for(let c=0;c<contador;c++){
         let randomCaracter = Math.floor(Math.random()*caracteres.length);
-        //variável que armazena um número que representará um caractere do array caracteres.
-        senha = caracteres[randomCaracter];    
-        recebeSenha = senha+recebeSenha;
+        recebeSenha = caracteres[randomCaracter]+recebeSenha;
     }    
     novaSenha.innerHTML = `Sua nova senha:<br> ${recebeSenha}`;
     senhasGeradas.appendChild(node);

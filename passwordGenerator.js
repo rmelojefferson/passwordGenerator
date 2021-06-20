be3f8fe1e081afa798f088e13b7d6a76e054ec0e
Jefferson R. Melo <82545165+rmelojefferson@users.noreply.github.com>
2021-06-20 11:35:04 -0300

    Adicionando funcionalidades e CSS
    Adicionando as funcionalidades:
    - Botão clean (Reload da página)
    - Botão copy (Botão copia a senha gerada no campo "Nova Senha")
    
    Estilo CSS:
    Folha de estilo CSS com tema Dark

--- passwordGenerator.js
+++ passwordGenerator.js
@@ -1,27 +1,45 @@
 let caracteres = `0123456789!@#$%¨&*abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'"()´{}[]~^?,.<>;/|+-`;
 
-let novaSenha = document.getElementById("newPassword");
-let senhasGeradas = document.getElementById("generatedPass");
-let botaoNovaSenha = document.getElementById("newPassButton");
-botaoNovaSenha.onclick = newPassButton;
 let recebeSenha = '';
 let contador = '';
 
+let senhasGeradas = document.getElementById("generatedPass");
+let botaoNovaSenha = document.getElementById("newPassButton");
+    botaoNovaSenha.onclick = newPassButton;
+
+let limpaSenha = document.getElementById("clean");
+    limpaSenha.onclick = cleanButton;
+
 function newPassButton(){
     let check = document.getElementById("passwordLength").checked;    
-    var node = document.createElement("ul");
     var senhaNode = document.createTextNode(recebeSenha);
-    node.appendChild(senhaNode);
+    var node = document.createElement("ul");
+        node.appendChild(senhaNode);
     if(check==true){
         contador = 8;
     }else{
         contador = 16;
     }
-    recebeSenha= '';
+
+    recebeSenha = '';
+    
     for(let c=0;c<contador;c++){
         let randomCaracter = Math.floor(Math.random()*caracteres.length);
-        recebeSenha = caracteres[randomCaracter]+recebeSenha;
-    }    
-    novaSenha.innerHTML = `Sua nova senha:<br> ${recebeSenha}`;
-    senhasGeradas.appendChild(node);
+            recebeSenha = caracteres[randomCaracter]+recebeSenha;
+    }
+
+    let novaSenha = document.getElementById("newPassword");
+        novaSenha.value = recebeSenha;
+        novaSenha.style.color = "#ba86fd";
+    
+        senhasGeradas.appendChild(node);
+        document.getElementById("suasSenhasButton").innerText = "Suas senhas:";
+    
+    let copyPass = document.getElementById("newPassword");
+        copyPass.select();
+        document.execCommand("copy");
+}
+
+function cleanButton( ){
+    location.reload()  
 }
